79c9ffc485b6b2c3dcc97e1efcbd7482f6e7acde
mcmah309 <dillonmcmahon79@gmail.com>
2025-09-09 05:47:34 +0000

    Add Initial implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,60 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::{backtrace::Backtrace, io::Write, marker::PhantomData, mem, ops::Deref, process::exit, sync::Arc};
+
+/// A scope that holds a reference with lifetime `'a` that be converted to a reference with a
+/// `'static` lifetime. Runtime checks are used to ensure that no `SArc` exists when this `Scope` is
+/// dropped.
+///
+/// UNDEFINED BEHAVIOR: It may cause undefined behavior to forget this value - `std::mem::forget(scope)`.
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+struct Scope<'a, T: 'static> {
+    data: Arc<&'static T>,
+    _scope: PhantomData<&'a ()>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl<'a, T: 'static> Scope<'a, T> {
+    fn new(value: &'a T) -> Self {
+        let value = unsafe { mem::transmute::<&'a T, &'static T>(value) };
+        let arc = Arc::new(value);
+        Scope {
+            data: arc,
+            _scope: std::marker::PhantomData,
+        }
+    }
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    /// Lifts reference with lifetime `'a` out of the scope into `'static` and relies on runtime
+    /// Checks to ensure safety.
+    fn lift(&self) -> SArc<T> {
+        SArc(self.data.clone())
+    }
+}
+
+impl<'a, T: 'static> Drop for Scope<'a, T> {
+    fn drop(&mut self) {
+        if std::sync::Arc::strong_count(&self.data) != 1 {
+            let bt = Backtrace::capture();
+            const ROOT_MSG: &str = "Fatal error: Scope dropped while SArc references still exist. \
+                This would cause undefined behavior. Aborting.\n";
+            let msg = match bt.status() {
+                std::backtrace::BacktraceStatus::Unsupported => ROOT_MSG.to_owned(),
+                std::backtrace::BacktraceStatus::Disabled => format!("{ROOT_MSG}\n(Hint: re-run with `RUST_BACKTRACE=1` to see a backtrace.)\n"),
+                std::backtrace::BacktraceStatus::Captured => format!("{ROOT_MSG}\nBacktrace:\n{bt}\n"),
+                _ => ROOT_MSG.to_owned(),
+            };
+
+            let _ = std::io::stderr().write_all(msg.as_bytes());
+            let _ = std::io::stderr().flush();
+            std::process::abort();
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+struct SArc<T: 'static>(Arc<&'static T>);
+
+impl<T: 'static> Deref for SArc<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        self.0.as_ref()
     }
 }
