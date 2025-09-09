8c2cd17c2474f218439b35ce7c0ef39a91147a9c
mcmah309 <dillonmcmahon79@gmail.com>
2025-09-09 07:48:49 +0000

    Add min_safety feature

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,32 @@
-use std::{marker::PhantomData, mem, ops::Deref, sync::Arc};
+#[cfg(any(feature = "min_safety", debug_assertions))]
+use std::sync::Arc;
+use std::{marker::PhantomData, mem, ops::Deref};
 
 /// A scope that holds a reference with lifetime `'a` that be converted to a reference with a
 /// `'static` lifetime. Runtime checks are used to ensure that no `SArc` exists when this `Scope` is
-/// dropped.
+/// dropped. If a `Scope` is dropped while any derived `SArc` exist. Then it will abort the whole
+/// program instead of panic. This is because `SArc` could exist on another thread and be unaffected
+/// by the panic or the panic could be recovered from. This could lead to undefined behavior.
 ///
-/// UNDEFINED BEHAVIOR: It may cause undefined behavior to forget this value - `std::mem::forget(scope)`.
+/// UNDEFINED BEHAVIOR: It may cause undefined behavior to forget this value (`std::mem::forget(scope)`),
+/// the `Drop` code must run to prevent undefined behavior. If the default `min_safety` flag is not
+/// enabled, this may cause undefined behavior in other scenarios during non-debug mode.
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct Scope<'a, T: 'static> {
+    #[cfg(any(feature = "min_safety", debug_assertions))]
     data: Arc<&'static T>,
+    #[cfg(not(any(feature = "min_safety", debug_assertions)))]
+    data: &'static T,
     _scope: PhantomData<&'a ()>,
 }
 
 impl<'a, T: 'static> Scope<'a, T> {
     pub fn new(value: &'a T) -> Self {
         let value = unsafe { mem::transmute::<&'a T, &'static T>(value) };
-        let arc = Arc::new(value);
+        #[cfg(any(feature = "min_safety", debug_assertions))]
+        let value = Arc::new(value);
         Scope {
-            data: arc,
+            data: value,
             _scope: std::marker::PhantomData,
         }
     }
@@ -24,51 +34,72 @@ impl<'a, T: 'static> Scope<'a, T> {
     /// Lifts reference with lifetime `'a` out of the scope into `'static` and relies on runtime
     /// Checks to ensure safety.
     pub fn lift(&self) -> SArc<T> {
-        SArc(self.data.clone())
+        #[cfg(any(feature = "min_safety", debug_assertions))]
+        return SArc(self.data.clone());
+        #[cfg(not(any(feature = "min_safety", debug_assertions)))]
+        return SArc(self.data);
     }
 }
 
 impl<'a, T: 'static> Drop for Scope<'a, T> {
     fn drop(&mut self) {
-        if std::sync::Arc::strong_count(&self.data) != 1 {
-            const ROOT_MSG: &str = "Fatal error: Scope dropped while SArc references still exist. \
+        #[cfg(any(feature = "min_safety", debug_assertions))]
+        {
+            if std::sync::Arc::strong_count(&self.data) != 1 {
+                const ROOT_MSG: &str = "Fatal error: Scope dropped while SArc references still exist. \
                 This would cause undefined behavior. Aborting.\n";
-            // We don't panic since panics can be recovered and panics also only effect a single thread
-            // While the value could have been sent to a different thread.
-            #[cfg(not(test))]
-            {
-                let bt = std::backtrace::Backtrace::capture();
-                let msg = match bt.status() {
-                    std::backtrace::BacktraceStatus::Unsupported => ROOT_MSG.to_owned(),
-                    std::backtrace::BacktraceStatus::Disabled => format!(
-                        "{ROOT_MSG}\n(Hint: re-run with `RUST_BACKTRACE=1` to see a backtrace.)\n"
-                    ),
-                    std::backtrace::BacktraceStatus::Captured => {
-                        format!("{ROOT_MSG}\nBacktrace:\n{bt}\n")
-                    }
-                    _ => ROOT_MSG.to_owned(),
-                };
-                use std::io::Write;
-                let _ = std::io::stderr().write_all(msg.as_bytes());
-                let _ = std::io::stderr().flush();
-                std::process::abort();
-            }
-            #[cfg(test)]
-            {
-                panic!("{}", ROOT_MSG);
+                // We don't panic since panics can be recovered and panics also only effect a single thread
+                // While the value could have been sent to a different thread.
+                #[cfg(not(test))]
+                {
+                    let bt = std::backtrace::Backtrace::capture();
+                    let msg = match bt.status() {
+                        std::backtrace::BacktraceStatus::Unsupported => ROOT_MSG.to_owned(),
+                        std::backtrace::BacktraceStatus::Disabled => format!(
+                            "{ROOT_MSG}\n(Hint: re-run with `RUST_BACKTRACE=1` to see a backtrace.)\n"
+                        ),
+                        std::backtrace::BacktraceStatus::Captured => {
+                            format!("{ROOT_MSG}\nBacktrace:\n{bt}\n")
+                        }
+                        _ => ROOT_MSG.to_owned(),
+                    };
+                    use std::io::Write;
+                    let _ = std::io::stderr().write_all(msg.as_bytes());
+                    let _ = std::io::stderr().flush();
+                    std::process::abort();
+                }
+                #[cfg(test)]
+                {
+                    panic!("{}", ROOT_MSG);
+                }
             }
         }
     }
 }
 
+#[cfg(any(feature = "min_safety", debug_assertions))]
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct SArc<T: 'static>(Arc<&'static T>);
 
+#[cfg(not(any(feature = "min_safety", debug_assertions)))]
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct SArc<T: 'static>(&'static T);
+
+#[cfg(any(feature = "min_safety", debug_assertions))]
 impl<T: 'static> Deref for SArc<T> {
     type Target = T;
 
     fn deref(&self) -> &Self::Target {
         self.0.as_ref()
+    }
+}
+
+#[cfg(not(any(feature = "min_safety", debug_assertions)))]
+impl<T: 'static> Deref for SArc<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        self.0
     }
 }
 
@@ -99,10 +130,15 @@ mod tests {
             let result = std::panic::catch_unwind(|| {
                 std::mem::drop(scope);
             });
-
+            #[cfg(any(feature = "min_safety", debug_assertions))]
             assert!(
                 result.is_err(),
                 "expected panic when dropping scope with live SArc"
+            );
+            #[cfg(not(any(feature = "min_safety", debug_assertions)))]
+            assert!(
+                result.is_ok(),
+                "This should show UB of disabling `min_safety` in release mode"
             );
         }
 
@@ -130,9 +166,15 @@ mod tests {
             let result = std::panic::catch_unwind(|| {
                 std::mem::drop(scope);
             });
+            #[cfg(any(feature = "min_safety", debug_assertions))]
             assert!(
                 result.is_err(),
                 "expected panic when dropping scope with live SArc in the task"
+            );
+            #[cfg(not(any(feature = "min_safety", debug_assertions)))]
+            assert!(
+                result.is_ok(),
+                "This should show UB of disabling `min_safety` in release mode"
             );
         }
 
