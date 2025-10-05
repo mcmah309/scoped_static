cd15798949248a9074d5a55c55d0cc442cbd4f2c
mcmah309 <dillonmcmahon79@gmail.com>
2025-10-05 02:43:40 +0000

    meta: Rename constructs

--- src/lib.rs
+++ src/lib.rs
@@ -2,19 +2,19 @@
 use std::sync::Arc;
 use std::{marker::PhantomData, mem, ops::Deref};
 
-/// A reference with lifetime `'a` that be lifted to a reference with a `'static` lifetime (`Lifted`).
-/// Runtime checks are used to ensure that no derived `Lifted` exists when this `Scoped` is
+/// A reference with lifetime `'a` that can be lifted to a reference with a `'static` lifetime ([`Scoped`]).
+/// Runtime checks are used to ensure that no derived [`Scoped`] exists when this [`ScopeGuard`] is
 /// dropped. 
 /// 
 /// ```rust
-/// use scope_lift::Scoped;
+/// use scoped_static::ScopeGuard;
 /// 
 /// #[tokio::main]
 /// async fn main() {
 ///     let concrete_value = Box::new(1.0);
 ///     let ref_value = &concrete_value;
-///     let scoped = Scoped::new(ref_value);
-///     let lifted = scoped.lift();
+///     let guard = ScopeGuard::new(ref_value);
+///     let lifted = guard.lift();
 ///     tokio::spawn(async move {
 ///         // Lifted is 'static so it can be moved into this closure that needs 'static
 ///         let value = **lifted + 1.0;
@@ -23,22 +23,22 @@ use std::{marker::PhantomData, mem, ops::Deref};
 ///     })
 ///     .await
 ///     .unwrap();
-///    // `scoped` is dropped here
+///    // `guard` is dropped here
 /// }
 /// ```
 /// 
-/// If a `Scoped` is dropped while any derived `Lifted` exist, then it will abort the whole
-/// program (instead of panic). This is because `Lifted` could exist on another thread and be unaffected
+/// If a [`ScopeGuard`] is dropped while any derived [`Scoped`] exist, then it will abort the whole
+/// program (instead of panic). This is because [`Scoped`] could exist on another thread and be unaffected
 /// by the panic or the panic could be recovered from. This could lead to undefined behavior.
 ///
-/// UNDEFINED BEHAVIOR: It may cause undefined behavior to forget this value (`std::mem::forget(scoped)`) -
+/// UNDEFINED BEHAVIOR: It may cause undefined behavior to forget this value (`std::mem::forget(guard)`) -
 /// the `Drop` code must run to prevent undefined behavior. 
 /// 
 /// UNDEFINED BEHAVIOR: If the *default* `min_safety` flag is not enabled, in non-debug mode, 
-/// this may cause undefined behavior if `Scoped` is drop before all derived `Lifted` are dropped.
+/// this may cause undefined behavior if [`ScopedGuard`] is drop before all derived [`Scoped`] are dropped.
 /// This is because there are no runtime safety checks in this scenario and the program will not abort.
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct Scoped<'a, T: 'static> {
+pub struct ScopeGuard<'a, T: 'static> {
     #[cfg(any(feature = "min_safety", debug_assertions))]
     data: Arc<&'static T>,
     #[cfg(not(any(feature = "min_safety", debug_assertions)))]
@@ -46,12 +46,12 @@ pub struct Scoped<'a, T: 'static> {
     _scope: PhantomData<&'a ()>,
 }
 
-impl<'a, T: 'static> Scoped<'a, T> {
+impl<'a, T: 'static> ScopeGuard<'a, T> {
     pub fn new(value: &'a T) -> Self {
         let value = unsafe { mem::transmute::<&'a T, &'static T>(value) };
         #[cfg(any(feature = "min_safety", debug_assertions))]
         let value = Arc::new(value);
-        Scoped {
+        ScopeGuard {
             data: value,
             _scope: std::marker::PhantomData,
         }
@@ -59,16 +59,16 @@ impl<'a, T: 'static> Scoped<'a, T> {
 
     /// Lifts this reference with lifetime `'a` into `'static` and relies on runtime
     /// checks to ensure safety.
-    pub fn lift(&self) -> Lifted<T> {
+    pub fn lift(&self) -> Scoped<T> {
         #[cfg(any(feature = "min_safety", debug_assertions))]
-        return Lifted(self.data.clone());
+        return Scoped(self.data.clone());
         #[cfg(not(any(feature = "min_safety", debug_assertions)))]
-        return Lifted(self.data);
+        return Scoped(self.data);
     }
 }
 
 #[cfg(any(feature = "min_safety", debug_assertions))]
-impl<'a, T> Deref for Scoped<'a, T> {
+impl<'a, T> Deref for ScopeGuard<'a, T> {
     type Target = T;
 
     fn deref(&self) -> &Self::Target {
@@ -77,7 +77,7 @@ impl<'a, T> Deref for Scoped<'a, T> {
 }
 
 #[cfg(not(any(feature = "min_safety", debug_assertions)))]
-impl<'a, T> Deref for Scoped<'a, T> {
+impl<'a, T> Deref for ScopeGuard<'a, T> {
     type Target = T;
 
     fn deref(&self) -> &Self::Target {
@@ -85,7 +85,7 @@ impl<'a, T> Deref for Scoped<'a, T> {
     }
 }
 
-impl<'a, T: 'static> Drop for Scoped<'a, T> {
+impl<'a, T: 'static> Drop for ScopeGuard<'a, T> {
     fn drop(&mut self) {
         #[cfg(any(feature = "min_safety", debug_assertions))]
         {
@@ -121,20 +121,20 @@ impl<'a, T: 'static> Drop for Scoped<'a, T> {
     }
 }
 
-/// A reference derived from a `Scoped`. The lifetime of the underlying
+/// A reference derived from a [`ScopeGuard`]. The lifetime of the underlying
 /// value has been lifted to `'static`. See `Scoped` for more info.
 #[cfg(any(feature = "min_safety", debug_assertions))]
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct Lifted<T: 'static>(Arc<&'static T>);
+pub struct Scoped<T: 'static>(Arc<&'static T>);
 
 /// A reference derived from a `Scoped`. The lifetime of the underlying
 /// value has been lifted to `'static`. See `Scoped` for more info.
 #[cfg(not(any(feature = "min_safety", debug_assertions)))]
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct Lifted<T: 'static>(&'static T);
+pub struct Scoped<T: 'static>(&'static T);
 
 #[cfg(any(feature = "min_safety", debug_assertions))]
-impl<T: 'static> Deref for Lifted<T> {
+impl<T: 'static> Deref for Scoped<T> {
     type Target = T;
 
     fn deref(&self) -> &Self::Target {
@@ -143,7 +143,7 @@ impl<T: 'static> Deref for Lifted<T> {
 }
 
 #[cfg(not(any(feature = "min_safety", debug_assertions)))]
-impl<T: 'static> Deref for Lifted<T> {
+impl<T: 'static> Deref for Scoped<T> {
     type Target = T;
 
     fn deref(&self) -> &Self::Target {
@@ -166,22 +166,22 @@ mod tests {
 
     #[cfg(test)]
     mod normal_tests {
-        use crate::{Scoped, tests::NonCopy};
+        use crate::{ScopeGuard, tests::NonCopy};
 
         #[test]
         fn dangling() {
             let concrete_value = Box::new(NonCopy::new());
             let ref_value = &concrete_value;
-            let scope = Scoped::new(ref_value);
-            let lifted = scope.lift();
+            let guard = ScopeGuard::new(ref_value);
+            let lifted = guard.lift();
             lifted.access_value();
             let result = std::panic::catch_unwind(|| {
-                std::mem::drop(scope);
+                std::mem::drop(guard);
             });
             #[cfg(any(feature = "min_safety", debug_assertions))]
             assert!(
                 result.is_err(),
-                "expected panic when dropping scope with live Lifted"
+                "expected panic when dropping ScopeGuard with an alive Scoped"
             );
             #[cfg(not(any(feature = "min_safety", debug_assertions)))]
             assert!(
@@ -194,30 +194,30 @@ mod tests {
         fn valid() {
             let concrete_value = Box::new(NonCopy::new());
             let ref_value = &concrete_value;
-            let scope = Scoped::new(ref_value);
-            let lifted = scope.lift();
+            let guard = ScopeGuard::new(ref_value);
+            let lifted = guard.lift();
             lifted.access_value();
             std::mem::drop(lifted);
-            std::mem::drop(scope);
+            std::mem::drop(guard);
         }
 
         #[tokio::test]
         async fn async_dangling() {
             let concrete_value = Box::new(NonCopy::new());
             let ref_value = &concrete_value;
-            let scope = Scoped::new(ref_value);
-            let lifted = scope.lift();
+            let guard = ScopeGuard::new(ref_value);
+            let lifted = guard.lift();
             lifted.access_value();
             tokio::spawn(async move {
                 lifted.access_value();
             });
             let result = std::panic::catch_unwind(|| {
-                std::mem::drop(scope);
+                std::mem::drop(guard);
             });
             #[cfg(any(feature = "min_safety", debug_assertions))]
             assert!(
                 result.is_err(),
-                "expected panic when dropping scope with live Lifted in the task"
+                "expected panic when dropping ScopeGuard with live Scoped in the task"
             );
             #[cfg(not(any(feature = "min_safety", debug_assertions)))]
             assert!(
@@ -230,30 +230,30 @@ mod tests {
         async fn async_valid() {
             let concrete_value = Box::new(NonCopy::new());
             let ref_value = &concrete_value;
-            let scope = Scoped::new(ref_value);
-            let lifted = scope.lift();
+            let guard = ScopeGuard::new(ref_value);
+            let lifted = guard.lift();
             lifted.access_value();
             tokio::spawn(async move {
                 lifted.access_value();
             })
             .await
             .unwrap();
-            std::mem::drop(scope);
+            std::mem::drop(guard);
         }
     }
 
     #[cfg(test)]
     mod ub_tests {
-        use crate::{Scoped, tests::NonCopy};
+        use crate::{ScopeGuard, tests::NonCopy};
 
         #[test]
         fn undefined_behavior() {
             let concrete_value = Box::new(NonCopy::new());
             let ref_value = &concrete_value;
-            let scope = Scoped::new(ref_value);
-            let lifted = scope.lift();
+            let guard = ScopeGuard::new(ref_value);
+            let lifted = guard.lift();
             lifted.access_value();
-            std::mem::forget(scope);
+            std::mem::forget(guard);
             std::mem::drop(concrete_value);
             let result = std::panic::catch_unwind(|| {
                 // The assert here should fail (Showing UB) in a testable way
@@ -261,7 +261,7 @@ mod tests {
             });
             assert!(
                 result.is_err(),
-                "Forgetting the scope, dropping the underlying, then accessing an Lifted value should be UB"
+                "Forgetting the ScopeGuard, dropping the underlying, then accessing an Scoped value should be UB"
             );
         }
 
@@ -269,8 +269,8 @@ mod tests {
         async fn async_undefined_behavior() {
             let concrete_value = Box::new(NonCopy::new());
             let ref_value = &concrete_value;
-            let scope = Scoped::new(ref_value);
-            let lifted = scope.lift();
+            let guard = ScopeGuard::new(ref_value);
+            let lifted = guard.lift();
             lifted.access_value();
             let fut = tokio::spawn(async move {
                 let result = std::panic::catch_unwind(|| {
@@ -279,12 +279,12 @@ mod tests {
                 });
                 result
             });
-            std::mem::forget(scope);
+            std::mem::forget(guard);
             std::mem::drop(concrete_value);
             let result = fut.await.unwrap();
             assert!(
                 result.is_err(),
-                "Forgetting the scope, dropping the underlying, then accessing an Lifted value should be UB"
+                "Forgetting the ScopeGuard, dropping the underlying, then accessing an Scoped value should be UB"
             );
         }
     }
